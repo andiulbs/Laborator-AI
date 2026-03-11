98a45d6aa694d5580e061aae8ea364f2055a8227
andyiH <hauerandrei@yahoo.com>
2026-03-11 17:55:37 +0200

    Lab1
    Numeric, Strings, Boolean

---
+++ Laborator1.py
@@ -0,0 +1,29 @@
+# type(1)
+# print('salut','crumbs',10)
+#
+# age=input('varsta')
+# varsta=1809
+
+
+print('I\'m thirsty')
+print("I'm thirsty")
+
+print(type(1.0))
+hello1='hello'.upper()
+print(hello1)
+print(hello1.upper())
+print()
+
+name1='andy'
+print(name1)
+
+print('cel mai tare lab')
+
+print('bye bye'.upper().replace('bye','sal'))
+
+
+bool = True
+bool2 = False
+print(bool(None))
+print(bool(0.0))
+
