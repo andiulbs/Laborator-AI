4ba3961e885c54e0f3daf7c4cca5fa97d4241f6b
andyiH <andrei.hauer@ulbsibiu.ro>
2026-03-18 17:43:24 +0200

    Lab2
    liste si exercitii

---
+++ Laborator2.py
@@ -0,0 +1,36 @@
+from itertools import repeat
+
+from jsonschema.benchmarks.contains import size
+from more_itertools.more import repeat_each
+
+my_list=[9,8,7,6,5,4,3,2,'salut',1]
+print(my_list)
+
+for my_list[1] in my_list:
+        print(my_list) # 1,2,3
+
+else:
+    print(my_list[3])
+
+    picture = [
+        [0, 0, 0, 1, 0, 0, 0],
+        [0, 0, 1, 1, 1, 0, 0],
+        [0, 1, 1, 1, 1, 1, 0],
+        [1, 1, 1, 1, 1, 1, 1],
+        [0, 0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0]]
+
+print(picture)
+
+print(list(range(9,10)))
+
+my_list2=[10,9,8,7,6,5,4,3,2,1]
+#for my_list2>8 in my_list2:
+   # print(my_list2[0,1])
+
+
+
+
+
+
+
